1bfd283a295ce55ba612b431db73480a7b82e6c7
Laszlo Balogh <laszlo@thebaloghs.com>
2016-11-15 00:11:00 -0700

    Iteration4: all features work, moved from Sinatra settings I3 back to using number_guesser class

--- number_guesser.rb
+++ number_guesser.rb
@@ -1,10 +1,13 @@
 class NumberGuesser
   attr_reader :secret_number,
               :highest,
-              :game_on
+              :game_on,
+              :max_guess,
+              :guess_count
 
-  def initialize(highest = 100)
+  def initialize(highest = 100, max_guess = 5)
     @highest = highest
+    @max_guess = max_guess
     @game_on = false
     start_game
   end
@@ -16,39 +19,64 @@ class NumberGuesser
   def start_game
     @secret_number = pick_secret_number
     @game_on = true
+    @guess_count = max_guess
   end
 
-  def way_too_high
-    {"feedback" => "Way too high!",
-    "secret_number" => ""}
+  def show_number(cheat)
+    return "" unless cheat
+    "The SECRET NUMBER is #{secret_number}."
   end
 
-  def too_high
-    {"feedback" => "Too high!",
-    "secret_number" => ""}
+  def way_too_high(cheat)
+    @guess_count -= 1
+    {:last_guess => "Way too high!",
+    :secret_number_message => show_number(cheat),
+    :color => "red"}
   end
 
-  def too_low
-    {"feedback" => "Too low!",
-    "secret_number" => ""}
+  def too_high(cheat)
+    @guess_count -= 1
+    {:last_guess => "Too high!",
+    :secret_number_message => show_number(cheat),
+    :color => "violet"}
   end
 
-  def way_too_low
-    {"feedback" => "Way too low!",
-    "secret_number" => ""}
+  def too_low(cheat)
+    @guess_count -= 1
+    {:last_guess => "Too low!",
+    :secret_number_message => show_number(cheat),
+    :color => "violet"}
+  end
+
+  def way_too_low(cheat)
+    @guess_count -= 1
+    {:last_guess => "Way too low!",
+    :secret_number_message => show_number(cheat),
+    :color => "red"}
   end
 
   def you_got_it_right
     @game_on = false
-    {"feedback" => "You got it right!",
-    "secret_number" => "The SECRET NUMBER is #{secret_number}"}
+    output = {:last_guess => "You WON!!! You got it right!",
+    :secret_number_message => "The SECRET NUMBER is #{secret_number}.",
+    :color => "green"}
+    start_game
+    output
   end
 
-  def evaluation(guess)
-    return way_too_high if guess.to_i > secret_number + 5
-    return too_high if guess.to_i > secret_number
-    return way_too_low if guess.to_i < secret_number - 5
-    return too_low if guess.to_i < secret_number
+  def you_lost(cheat)
+    start_game
+    {:last_guess => "You lost! I thought of a new number now...",
+    :secret_number_message => show_number(cheat),
+    :color => "red"}
+  end
+
+  def evaluation(guess, cheat)
+    return you_lost(cheat) if guess_count == 0
+    return way_too_high(cheat) if guess.to_i > secret_number + 5
+    return too_high(cheat) if guess.to_i > secret_number
+    return way_too_low(cheat) if guess.to_i < secret_number - 5
+    return too_low(cheat) if guess.to_i < secret_number
     you_got_it_right
   end
 
--- web_guesser.rb
+++ web_guesser.rb
@@ -1,26 +1,12 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'number_guesser'
 
-SECRET_NUMBER = rand(101)
-
-set :secret_number_empty, ""
-set :secret_number, Proc.new {"The SECRET NUMBER is " + SECRET_NUMBER}
-set :too_high, "Too high!"
-set :way_too_high, "Way too high!"
-set :too_low, "Too low!"
-set :way_too_low, "Way too low!"
-set :you_got_it_right, "You got it right!"
+number_guesser = NumberGuesser.new
 
 get "/" do
-  guess = params["guess"].to_i
-  feedback = {}
-  feedback[:secret_number_message] = settings.secret_number_empty
-  feedback[:last_guess] = settings.too_high if guess > SECRET_NUMBER
-  feedback[:last_guess] = settings.way_too_high if guess > SECRET_NUMBER + 5
-  feedback[:last_guess] = settings.too_low if guess < SECRET_NUMBER
-  feedback[:last_guess] = settings.way_too_low if guess < SECRET_NUMBER - 5
-  feedback[:last_guess] = settings.you_got_it_right if guess == SECRET_NUMBER
-  feedback[:secret_number_message] = settings.secret_number_empty if guess == SECRET_NUMBER
-  erb:index, :locals => {:last_guess => feedback[:last_guess], :secret_number_message => feedback[:secret_number_message]}
+  guess_evaluation = number_guesser.evaluation(params["guess"].to_i, params["cheat"])
+  erb:index, :locals => {:last_guess => guess_evaluation[:last_guess],
+                         :secret_number_message => guess_evaluation[:secret_number_message],
+                         :color => guess_evaluation[:color]}
 end
-
