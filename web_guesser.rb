db79a4aa09c6e208ecebf0b17cc95153160d68eb
Laszlo Balogh <laszlo@thebaloghs.com>
2016-11-14 23:28:15 -0700

    I3: using Sinatra settings feature for bonus points...

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,12 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative 'number_guesser'
 
-number_guesser = NumberGuesser.new
+SECRET_NUMBER = rand(101)
+
+set :secret_number_empty, ""
+set :secret_number, Proc.new {"The SECRET NUMBER is " + SECRET_NUMBER}
+set :too_high, "Too high!"
+set :way_too_high, "Way too high!"
+set :too_low, "Too low!"
+set :way_too_low, "Way too low!"
+set :you_got_it_right, "You got it right!"
 
 get "/" do
-  guess_evaluation = number_guesser.evaluation(params["guess"])
-  # throw guess_evaluation.inspect
-  erb:index, :locals => {:last_guess => guess_evaluation["feedback"], :secret_number_message => guess_evaluation["secret_number"]}
+  guess = params["guess"].to_i
+  feedback = {}
+  feedback[:secret_number_message] = settings.secret_number_empty
+  feedback[:last_guess] = settings.too_high if guess > SECRET_NUMBER
+  feedback[:last_guess] = settings.way_too_high if guess > SECRET_NUMBER + 5
+  feedback[:last_guess] = settings.too_low if guess < SECRET_NUMBER
+  feedback[:last_guess] = settings.way_too_low if guess < SECRET_NUMBER - 5
+  feedback[:last_guess] = settings.you_got_it_right if guess == SECRET_NUMBER
+  feedback[:secret_number_message] = settings.secret_number_empty if guess == SECRET_NUMBER
+  erb:index, :locals => {:last_guess => feedback[:last_guess], :secret_number_message => feedback[:secret_number_message]}
 end
 
